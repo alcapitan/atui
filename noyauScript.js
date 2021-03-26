dbc4065384489a124900367d20e3e759ff787215
Al Capitan <60216981+alcapitan@users.noreply.github.com>
2021-03-26 20:55:58 +0100

    Delete noyauScript.js

--- noyauScript.js
+++
@@ -1,9 +0,0 @@
-/* Barre de chargement */
-
-var readyState = document.readyState;
-objetChargement = document.getElementById("chargement");
-console.log(readyState);
-if (document.readyState === 'loading')
-{
-     objetChargement.style.height = "0px";
-}
