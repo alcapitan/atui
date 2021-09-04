b1a4e36627c3ea492c2f487b195e04580089ad24
Al Capitan <alexandre.boyer29@gmail.com>
2021-09-04 18:40:26 +0200

    Amélioration de l'audio player

--- atuiScriptAudioPlayer.js
+++ atuiScriptAudioPlayer.js
@@ -1,8 +1,10 @@
+var atuiAudioPlayer = document.getElementById('atuiAudioPlayer');
+var atuiAudioPlayerMusic = document.getElementById('atuiAudioPlayerMusic');
+
 /* Fermer Audio Player */
 
 function atuiAudioPlayerFermer()
 {
-	var atuiAudioPlayer = document.getElementById('atuiAudioPlayer');
 	atuiAudioPlayer.style.display = "none";
 }
 
@@ -10,89 +12,102 @@ function atuiAudioPlayerFermer()
 
 function atuiAudioPlayerMusicPlayPause()
 {
-	console.log("Coucou Milan !");
-	var atuiAudioPlayer = document.getElementById('atuiAudioPlayer');
 	atuiAudioPlayer.style.display = 'block';
-	var atuiAudioPlayerMusic = document.getElementById('atuiAudioPlayerMusic');
 	var atuiAudioPlayerControlsButtonsPlayPause = document.getElementById('atuiAudioPlayerControlsButtonsBasic').childNodes[3];
 	if (atuiAudioPlayerMusic.paused)
 	{
  		atuiAudioPlayerMusic.play();
-		atuiAudioPlayerControlsButtonsPlayPause.src = 'medias/icons/play.png';
+		atuiAudioPlayerControlsButtonsPlayPause.src = 'medias/icons/pause.png';
  	}
 	else
 	{
 		atuiAudioPlayerMusic.pause();
-		atuiAudioPlayerControlsButtonsPlayPause.src = 'medias/icons/pause.png';
+		atuiAudioPlayerControlsButtonsPlayPause.src = 'medias/icons/play.png';
 	}
 }
 
-/* Updates des métadonnées de l'Audio Player sur la musique en cours */
+/* Gestionnaire métadonnées */
 
-var player = document.getElementById('player');
-player.style.display = 'none';
-let choix = 0;
-var music = document.getElementById('audioPlayer');
-var player = document.querySelector('#audioPlayer');
-function chooseMusic(choose)
+var atuiAudioPlayerMusicProgress = "";
+function atuiAudioPlayerMusicChange(atuiAudioPlayerMusicChosen)
 {
-	if (choix != choose)
+	if (atuiAudioPlayerMusicChosen != atuiAudioPlayerMusicProgress)
 	{
-		choix = choose;
-		audioPlayer.src = 'musiques/' + artiste[choix - 1] + ' - ' + titre[choix - 1] + '.mp3';
-		console.log('La musique est : ' + audioPlayer.src);
-		var details = [album[choix - 1], date[choix - 1], titre[choix - 1], artiste[choix - 1]];
-		document.getElementById('nameAlbum').innerHTML = details[0];
-		document.getElementById('dateAlbum').innerHTML = details[1];
-		document.getElementById('titreMusic').innerHTML = details[2];
-		document.getElementById('artisteMusic').innerHTML = details[3];
-		document.getElementById('imgAlbum').src = 'musiques/images/' + choix + '.jpg';
+		atuiAudioPlayerMusicProgress = atuiAudioPlayerMusicChosen;
+		var atuiAudioPlayerMusicMetadata = [atuiAudioPlayerMusicArtiste[choix - 1], atuiAudioPlayerMusicTitre[choix - 1], atuiAudioPlayerMusicAlbum[choix - 1], atuiAudioPlayerMusicDate[choix - 1]];
+		document.getElementById("atuiAudioPlayerMusic").src = "medias/musics/test.mp3"; /* Musique */
+		document.getElementById("atuiAudioPlayer").childNodes[8].src = 'musiques/images/' + choix + '.jpg'; /* Image */
+		document.getElementById("atuiAudioPlayerInfosMusic").childNodes[1].innerHTML = atuiAudioPlayerMusicMetadata[0]; /* Artiste */
+		document.getElementById("atuiAudioPlayerInfosMusic").childNodes[3].innerHTML = atuiAudioPlayerMusicMetadata[1]; /* Titre */
+		document.getElementById("atuiAudioPlayerInfosMusicAdvanced").childNodes[1].innerHTML = atuiAudioPlayerMusicMetadata[2]; /* Album */
+		document.getElementById("atuiAudioPlayerInfosMusicAdvanced").childNodes[3].innerHTML = atuiAudioPlayerMusicMetadata[3]; /* Date */
 	}
 	play('audioPlayer');
 }
 
-/* Changement time play music */
+/* Change progress time */
 
-/* function update(player)
+function calculTime(time, duration)
+{
+	var hours = Math.floor(time / 3600);
+	var mins  = Math.floor((time % 3600) / 60);
+ 	var secs  = Math.floor(time % 60);
+	if (secs < 10)
+	{
+		secs = "0" + secs;
+	}
+	if (hours)
+	{
+		if (mins < 10)
+		{
+			mins = "0" + mins;
+		}
+		return hours + ":" + mins + ":" + secs; // hh:mm:ss
+	}
+	else
+	{
+		return mins + ":" + secs; // mm:ss
+	}
+}
+
+function update(player)
 {
 	var duration = player.duration;    // Durée totale
 	var time     = player.currentTime; // Temps écoulé
 	var fraction = time / duration;
 	var percent  = Math.ceil(fraction * 100);
-	var progress = document.querySelector('#progressBar');
+	var progress = document.getElementById("atuiAudioPlayerControlsProgress").childNodes[1];
 	progress.style.width = percent + '%';
-	document.querySelector('#progressTime').textContent = calculTime(time);
-	document.querySelector('#durationTime').textContent = calculTime(duration);
+	document.getElementById("atuiAudioPlayerControlsTime").childNodes[1].textContent = calculTime(time);
+	document.getElementById("atuiAudioPlayerControlsTime").childNodes[5].textContent = calculTime(duration);
 }
 
 function getPosition(element){
-    var top = 0, left = 0;
-    
-    do {
-        top  += element.offsetTop;
-        left += element.offsetLeft;
-    } while (element = element.offsetParent);
-    
-    return { x: left, y: top };
-}
-
-function getMousePosition(event) {
-    return {
-        x: event.pageX,
-        y: event.pageY
-    };
-} */
-
-function clickProgress(idPlayer, control, event) {
-    var parent = getPosition(control);    // La position absolue de la progressBar
-    var target = getMousePosition(event); // L'endroit de la progressBar où on a cliqué
-    var player = document.querySelector('#' + idPlayer);
-  
-    var x = target.x - parent.x; 
-    var wrapperWidth = document.querySelector('#progressBarControl').offsetWidth;
-    
-    var percent = Math.ceil((x / wrapperWidth) * 100);    
-    var duration = player.duration;
-    
-    player.currentTime = (duration * percent) / 100;
-}
+	var top = 0, left = 0;
+	
+	do {
+	    top  += element.offsetTop;
+	    left += element.offsetLeft;
+	} while (element = element.offsetParent);
+	
+	return { x: left, y: top };
+ }
+ function getMousePosition(event) {
+	return {
+	    x: event.pageX,
+	    y: event.pageY
+	};
+ }
+ 
+ function atuiAudioPlayerControlsProgressChange(control, event) {
+	var parent = getPosition(control);    // La position absolue de la progressBar
+	var target = getMousePosition(event); // L'endroit de la progressBar où on a cliqué
+   
+	var x = target.x - parent.x; 
+	var wrapperWidth = document.getElementById('atuiAudioPlayerControlsProgress').childNodes[1].offsetWidth;
+	
+	var percent = Math.ceil((x / wrapperWidth) * 100);    
+	var duration = atuiAudioPlayerMusic.duration;
+	
+	atuiAudioPlayerMusic.currentTime = (duration * percent) / 100;
+ }
