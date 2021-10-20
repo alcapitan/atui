b45f9709fd387cefd09a241f9f63f356aceec7c7
Al Capitan <alexandre.boyer29@gmail.com>
2021-10-20 18:33:35 +0200

    Travail du 20/10/21 - début atuiNotifications - correction de bogues d'affichages

--- atuiScriptNotifications.js
+++ atuiScriptNotifications.js
@@ -1,1 +1,18 @@
-/* Coucou Milan ! */
+var centerNotification = document.getElementById("atuiNotificationsCenter");
+
+/* Messages */
+
+function notificationMessage(title,text)
+{
+    var elementNotification = document.createElement('aside');
+    elementNotification.classList.add("atuiNotifications")
+    var elementNotificationTitle = document.createElement('h3');
+    elementNotificationTitle.textContent = 'Title';
+    var elementNotificationText = document.createElement('p');
+    elementNotificationText.textContent = 'Text. Coucou Milan !';
+    elementNotification.appendChild(elementNotificationTitle);
+    elementNotification.appendChild(elementNotificationText);
+    centerNotification.appendChild(elementNotification);
+}
+
+notificationMessage();
