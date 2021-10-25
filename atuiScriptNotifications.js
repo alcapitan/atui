7f8f1579e3bb6d4cc1f0dac800faf3c13f98665d
Al Capitan <alexandre.boyer29@gmail.com>
2021-10-25 18:46:08 +0200

    Travail sur atuiNotifications

--- atuiScriptNotifications.js
+++ atuiScriptNotifications.js
@@ -7,12 +7,20 @@ function notificationMessage(title,text)
     var elementNotification = document.createElement('aside');
     elementNotification.classList.add("atuiNotifications")
     var elementNotificationTitle = document.createElement('h3');
-    elementNotificationTitle.textContent = 'Title';
+    elementNotificationTitle.textContent = title;
     var elementNotificationText = document.createElement('p');
-    elementNotificationText.textContent = 'Text. Coucou Milan !';
+    elementNotificationText.textContent = text;
+    var elementNotificationButtonOk = document.createElement('button');
+    elementNotificationButtonOk.textContent = "Ok";
+    var elementNotificationButtonBtnFermer = document.createElement('img');
+    elementNotificationButtonBtnFermer.setAttribute('src','medias/icons/fermer fenêtre.png');
+    elementNotificationButtonBtnFermer.getAttribute('src');
     elementNotification.appendChild(elementNotificationTitle);
     elementNotification.appendChild(elementNotificationText);
+    elementNotification.appendChild(elementNotificationButtonOk);
+    elementNotification.appendChild(elementNotificationBtnFermer);
     centerNotification.appendChild(elementNotification);
 }
 
-notificationMessage();
+notificationMessage('1e notif','Coucou..');
+notificationMessage('2e notif','.. Milan !');
