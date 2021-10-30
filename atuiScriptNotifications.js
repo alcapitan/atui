38a86c8295bb02f5e7b504271984278f8fa9f52f
Alex <alexandre.boyer29@gmail.com>
2021-10-30 22:31:18 +0200

    Travail sur atuiNotifications

--- atuiScriptNotifications.js
+++ atuiScriptNotifications.js
@@ -4,22 +4,51 @@ var centerNotification = document.getElementById("atuiNotificationsCenter");
 
 function notificationMessage(title,text)
 {
+    // Créer boite notification
     var elementNotification = document.createElement('aside');
-    elementNotification.classList.add("atuiNotifications")
+    elementNotification.classList.add("atuiNotifications");
+
+    // Titre
     var elementNotificationTitle = document.createElement('h3');
     elementNotificationTitle.textContent = title;
+
+    // Texte
     var elementNotificationText = document.createElement('p');
     elementNotificationText.textContent = text;
-    var elementNotificationButtonOk = document.createElement('button');
-    elementNotificationButtonOk.textContent = "Ok";
-    var elementNotificationButtonBtnFermer = document.createElement('img');
-    elementNotificationButtonBtnFermer.setAttribute('src','medias/icons/fermer fenêtre.png');
-    elementNotificationButtonBtnFermer.getAttribute('src');
+
+    // Buttons
+    var elementNotificationButtonsContainer = document.createElement('div');
+    elementNotificationButtonsContainer.classList.add("atuiNotificationsButtonsContainer");
+
+    var elementNotificationButtonAgree = document.createElement('button');
+    elementNotificationButtonAgree.textContent = "J'accepte";
+    elementNotificationButtonAgree.onclick = "a";
+    elementNotificationButtonAgree.classList.add("atuiNotificationsButtonMain");
+
+    var elementNotificationButtonDisagreeOne = document.createElement('button');
+    elementNotificationButtonDisagreeOne.textContent = "Je refuse une fois";
+
+    var elementNotificationButtonDisagreeAlways = document.createElement('button');
+    elementNotificationButtonDisagreeAlways.textContent = "Je refuse pour toujours";
+
+    // Icone Fermer
+    var elementNotificationButtonFermer = document.createElement('img');
+    elementNotificationButtonFermer.src = 'medias/icons/fermer fenêtre.png';
+    elementNotificationButtonFermer.classList.add("atuiNotificationsButtonFermer");
+
+    // Son Notification
+    var atuiNotificationSound = new Audio('medias/musics/atuiNotification.mp3');
+
+    // Insertion de l'élément
+    elementNotification.appendChild(elementNotificationButtonFermer);
     elementNotification.appendChild(elementNotificationTitle);
     elementNotification.appendChild(elementNotificationText);
-    elementNotification.appendChild(elementNotificationButtonOk);
-    elementNotification.appendChild(elementNotificationBtnFermer);
+    elementNotification.appendChild(elementNotificationButtonsContainer);
+    elementNotificationButtonsContainer.appendChild(elementNotificationButtonAgree);
+    elementNotificationButtonsContainer.appendChild(elementNotificationButtonDisagreeOne);
+    elementNotificationButtonsContainer.appendChild(elementNotificationButtonDisagreeAlways);
     centerNotification.appendChild(elementNotification);
+    atuiNotificationSound.play();
 }
 
 notificationMessage('1e notif','Coucou..');
