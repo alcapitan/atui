c42c913f8094426507adbf264acaf47d9047d83b
Al Capitan <alexandre.boyer29@gmail.com>
2022-02-08 16:05:29 +0100

    Améliorations aside dans header + Ajout panneau searchbar

--- atui/SearchService/main.js
+++ atui/SearchService/main.js
@@ -4,3 +4,22 @@ Nom du module : Search Service
 Version : dev2
 */
 
+
+searchBarHeaderZone = document.getElementById("atuiSearchServices_Header");
+searchBarHeaderBar = document.getElementById("atuiSearchServices_Header").childNodes[1];
+searchBarHeaderExtras = document.getElementById("atuiSearchServices_Header").childNodes[3];
+
+function atuiSearchService_HeaderDevelop()
+{
+     searchBarHeaderBar.style.width = "calc(100% - 20px)";
+     searchBarHeaderBar.style.margin = "10px";
+     searchBarHeaderExtras.style.display = "block";
+}
+
+function atuiSearchService_HeaderAntidevelop()
+{
+     searchBarHeaderBar.style.width = "100%";
+     searchBarHeaderBar.style.margin = "0px";
+     searchBarHeaderExtras.style.display = "none";
+}
+
