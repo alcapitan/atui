7a049e0dd1e7f23893031c4f6ba2a097e5f5db38
Al Capitan <alexandre.boyer29@gmail.com>
2022-02-23 16:53:56 +0100

    Retour ancien script Selector

--- atui/kernel/main.js
+++ atui/kernel/main.js
@@ -194,8 +194,8 @@ function atuiKernel_ToolsSelectorDisplay(element,wish)
 {
      cible = element.childNodes[3];
      element = element.childNodes[1];
-     offsetTop = element.getBoundingClientRect().y + 20;
-     offsetLeft = element.getBoundingClientRect().offsetLeft;
+     offsetTop = element.offsetTop + 20;
+     offsetLeft = element.offsetLeft;
      if (wish == false)
      {
           cible.style.visibility = "hidden";
@@ -206,7 +206,6 @@ function atuiKernel_ToolsSelectorDisplay(element,wish)
      {
           element.style.fontWeight = "bold";
           element.style.textDecoration = "underline";
-          /* Normal que ça marche pas, j'ai voulu mettre en commentaire après
           if (offsetLeft + cible.offsetWidth > document.body.clientWidth)
           {
                offsetLeft = document.body.clientWidth - cible.offsetWidth - 20;
@@ -214,7 +213,7 @@ function atuiKernel_ToolsSelectorDisplay(element,wish)
           if (offsetTop + cible.offsetHeight > window.innerHeight)
           {
                offsetTop = window.innerHeight - cible.offsetHeight - 20;
-          }*/
+          }
           offsetTop = offsetTop + "px";
           offsetLeft = offsetLeft + "px";
           cible.style.top = offsetTop;
