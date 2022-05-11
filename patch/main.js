11e60794619348c0a1a7291658f4b56689ac87bf
Al Capitan <alexandre.boyer29@gmail.com>
2022-05-11 19:04:06 +0200

    Finition de l'atomisation du CSS du kernel

--- patch/main.js
+++ patch/main.js
@@ -1,2 +1,2 @@
 /*atuiKernel_NotificationDisplay('alert','default',["console.log('ok')"],["console.log('annuler')"],'Maecenas dictum felis tristique lectus imperdiet aliquet. ','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum, turpis vel efficitur dictum, ipsum tortor pharetra velit, at mollis nisl ligula pellentesque mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ');*/
-atuiKernel_FooterLastedited(8,5,2021);
+atuiKernel_FooterLastedited(11,5,2021);
